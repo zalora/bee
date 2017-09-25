4079917a745b8101639319cbec54b536f0071f3f
ibigbug <akabyw@gmail.com>
2017-09-25 12:25:07 +0800

    fix doc gen

--- g_docs.go
+++ g_docs.go
@@ -805,7 +805,7 @@ func typeAnalyser(f *ast.Field) (isSlice bool, realType, swaggerType string) {
 		if isBasicType(val) {
 			return false, "map", basicTypes[val]
 		}
-		return false, val, "object"
+		return false, strings.TrimSuffix(strings.Split(val, " ")[1], "}"), "object"
 	}
 	if k, ok := basicTypes[fmt.Sprint(f.Type)]; ok {
 		return false, fmt.Sprint(f.Type), k
