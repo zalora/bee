aba253a7c2a674fa6412bdab333ee22fadc60511
Frank Meyer <frank.meyer@zalora.com>
2020-02-11 10:27:47 +0800

    add support for go modules
    Removed the checking of GOPATH at all and deleted related
    functions since `bee` and `beego` respectively is only
    used in Doraemon.

--- run.go
+++ run.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	path "path/filepath"
@@ -70,46 +69,27 @@ func init() {
 func runApp(cmd *Command, args []string) int {
 	ShowShortVersionBanner()
 
-	if len(args) == 0 || args[0] == "watchall" {
-		currpath, _ = os.Getwd()
+	currentGoPath, _ = os.Getwd()
+	appname = path.Base(currentGoPath)
+	ColorLog("[INFO] Using '%s' as 'appname'\n", appname)
 
-		if found, _gopath, _ := SearchGOPATHs(currpath); found {
-			appname = path.Base(currpath)
-			currentGoPath = _gopath
-		} else {
-			exitPrint(fmt.Sprintf("Bee does not support non Beego project: %s", currpath))
-		}
-		ColorLog("[INFO] Using '%s' as 'appname'\n", appname)
-	} else {
-		// Check if passed Bee application path/name exists in the GOPATH(s)
-		if found, _gopath, _path := SearchGOPATHs(args[0]); found {
-			currpath = _path
-			currentGoPath = _gopath
-			appname = path.Base(currpath)
-		} else {
-			panic(fmt.Sprintf("No Beego application '%s' found in your GOPATH", args[0]))
-		}
-
-		ColorLog("[INFO] Using '%s' as 'appname'\n", appname)
-
-		if strings.HasSuffix(appname, ".go") && isExist(currpath) {
-			ColorLog("[WARN] The appname is in conflict with currpath's file, do you want to build appname as %s\n", appname)
-			ColorLog("[INFO] Do you want to overwrite it? [yes|no]]  ")
-			if !askForConfirmation() {
-				return 0
-			}
+	if strings.HasSuffix(appname, ".go") && isExist(currpath) {
+		ColorLog("[WARN] The appname is in conflict with currpath's file, do you want to build appname as %s\n", appname)
+		ColorLog("[INFO] Do you want to overwrite it? [yes|no]]  ")
+		if !askForConfirmation() {
+			return 0
 		}
 	}
 
 	Debugf("current path:%s\n", currpath)
 
-	if runmode == "prod" || runmode == "dev"{
+	if runmode == "prod" || runmode == "dev" {
 		os.Setenv("BEEGO_RUNMODE", runmode)
 		ColorLog("[INFO] Using '%s' as 'runmode'\n", os.Getenv("BEEGO_RUNMODE"))
-	}else if runmode != ""{
+	} else if runmode != "" {
 		os.Setenv("BEEGO_RUNMODE", runmode)
 		ColorLog("[WARN] Using '%s' as 'runmode'\n", os.Getenv("BEEGO_RUNMODE"))
-	}else if os.Getenv("BEEGO_RUNMODE") != ""{
+	} else if os.Getenv("BEEGO_RUNMODE") != "" {
 		ColorLog("[WARN] Using '%s' as 'runmode'\n", os.Getenv("BEEGO_RUNMODE"))
 	}
 
