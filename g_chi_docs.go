59db3996997f0714cc2cda0ef3df63181a80586c
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2023-02-28 16:25:58 +0700

    DOR-4234: Validate route with chiDefinedRoutes

--- g_chi_docs.go
+++ g_chi_docs.go
@@ -11,6 +11,17 @@ import (
 	"github.com/astaxie/beego/swagger"
 )
 
+const (
+	basePath = "/v1"
+)
+
+var (
+	httpMethods = []string{
+		"Get", "Head", "Post", "Put", "Patch",
+		"Delete", "Connect", "Options", "Trace",
+	}
+)
+
 func generateChiDocs(curpath string) error {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(
@@ -31,7 +42,13 @@ func generateChiDocs(curpath string) error {
 		analisyscontrollerPkg(localName, im.Path.Value)
 	}
 
+	definedRoutes := chiDefinedRoutes(f)
 	for rt, item := range chiAPIs {
+		if !isValidRoute(rt, definedRoutes) {
+			ColorLog("[WARN] %s is not a valid route\n", rt)
+			continue
+		}
+
 		baseURLSplit := strings.Split(rt, "/")
 		if len(baseURLSplit) <= 1 {
 			continue
@@ -58,12 +75,40 @@ func generateChiDocs(curpath string) error {
 	return nil
 }
 
+func isValidRoute(route string, definedRoutes map[string]struct{}) bool {
+	route = path.Join(basePath, route)
+	_, ok := definedRoutes[route]
+	return ok
+}
+
 func appendTag(op *swagger.Operation, tag string) {
 	if op == nil {
 		return
 	}
 
 	op.Tags = append(op.Tags, tag)
+}
+
+func chiDefinedRoutes(node *ast.File) map[string]struct{} {
+	for _, decl := range node.Decls {
+		funcDecl, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+
+		if funcDecl.Name.Name != "New" {
+			continue
+		}
+
+		funcLit, err := findMuxGroupArg(funcDecl)
+		if err != nil {
+			continue
+		}
+
+		return traverseRoutes(funcLit, "")
+	}
+
+	return nil
 }
 
 func generateChiTags(node *ast.File, fset *token.FileSet) []swagger.Tag {
@@ -78,7 +123,7 @@ func generateChiTags(node *ast.File, fset *token.FileSet) []swagger.Tag {
 			continue
 		}
 
-		funcLit, err := findMuxGroupArg(funcDecl, fset)
+		funcLit, err := findMuxGroupArg(funcDecl)
 		if err != nil {
 			continue
 		}
@@ -105,7 +150,7 @@ func generateChiTags(node *ast.File, fset *token.FileSet) []swagger.Tag {
 	return tags
 }
 
-func assertCallExpression(callExpr *ast.CallExpr, object, function string) bool {
+func assertCallExpression(callExpr *ast.CallExpr, object string, functions []string) bool {
 	selExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
 	if !ok {
 		return false
@@ -124,11 +169,13 @@ func assertCallExpression(callExpr *ast.CallExpr, object, function string) bool 
 		return false
 	}
 
-	if selExpr.Sel.Name != function {
-		return false
+	for _, function := range functions {
+		if selExpr.Sel.Name == function {
+			return true
+		}
 	}
 
-	return true
+	return false
 }
 
 func extractLineRouteMap(callExpr *ast.CallExpr, fset *token.FileSet) map[int]string {
@@ -184,7 +231,7 @@ func extractLineRouteMap(callExpr *ast.CallExpr, fset *token.FileSet) map[int]st
 	return lineRouteMap
 }
 
-func findMuxGroupArg(funcDecl *ast.FuncDecl, fset *token.FileSet) (*ast.FuncLit, error) {
+func findMuxGroupArg(funcDecl *ast.FuncDecl) (*ast.FuncLit, error) {
 	for _, stmt := range funcDecl.Body.List {
 		expr, ok := stmt.(*ast.ExprStmt)
 		if !ok {
@@ -197,7 +244,7 @@ func findMuxGroupArg(funcDecl *ast.FuncDecl, fset *token.FileSet) (*ast.FuncLit,
 		}
 
 		// mux.Group(func(r chi.Router) { ... }
-		if !assertCallExpression(callExpr, "mux", "Group") {
+		if !assertCallExpression(callExpr, "mux", []string{"Group"}) {
 			continue
 		}
 
@@ -216,6 +263,48 @@ func findMuxGroupArg(funcDecl *ast.FuncDecl, fset *token.FileSet) (*ast.FuncLit,
 	return nil, errors.New("mux group arg is not found")
 }
 
+func traverseRoutes(funcLit *ast.FuncLit, route string) map[string]struct{} {
+	routes := make(map[string]struct{})
+	for _, stmt := range funcLit.Body.List {
+		expr, ok := stmt.(*ast.ExprStmt)
+		if !ok {
+			continue
+		}
+
+		callExpr, ok := expr.X.(*ast.CallExpr)
+		if !ok {
+			continue
+		}
+
+		if assertCallExpression(callExpr, "r", []string{"Route"}) {
+			localRoute := route + strings.Trim(callExpr.Args[0].(*ast.BasicLit).Value, `"`)
+			localRoute = strings.TrimRight(localRoute, `/`)
+			localRoute = toSwaggerPathKey(localRoute)
+			subRoutes := traverseRoutes(callExpr.Args[1].(*ast.FuncLit), localRoute)
+			for k, v := range subRoutes {
+				routes[k] = v
+			}
+			continue
+		}
+
+		if assertCallExpression(callExpr, "r", httpMethods) {
+			localRoute := route + "/" + strings.Trim(callExpr.Args[0].(*ast.BasicLit).Value, `"/`)
+			localRoute = strings.TrimRight(localRoute, `/`)
+			localRoute = toSwaggerPathKey(localRoute)
+			routes[localRoute] = struct{}{}
+			continue
+		}
+	}
+
+	return routes
+}
+
+func toSwaggerPathKey(path string) string {
+	path = strings.ReplaceAll(path, "{", ":")
+	path = strings.ReplaceAll(path, "}", "")
+	return path
+}
+
 func findRouteCall(funcLit *ast.FuncLit, fset *token.FileSet) (*ast.CallExpr, error) {
 	for _, stmt := range funcLit.Body.List {
 		expr, ok := stmt.(*ast.ExprStmt)
@@ -229,7 +318,7 @@ func findRouteCall(funcLit *ast.FuncLit, fset *token.FileSet) (*ast.CallExpr, er
 		}
 
 		// r.Route("v1", func(r chi.Router) { ... })
-		if !assertCallExpression(callExpr, "r", "Route") {
+		if !assertCallExpression(callExpr, "r", []string{"Route"}) {
 			continue
 		}
 
